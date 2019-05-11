6e645219add512271305d20fda085619ca9c391a
SealabJaster <sealabjaster@gmail.com>
2019-05-11 06:00:48 +0100

    Create a tag helper to simplify reproduction flag inputs

---
+++ Livestock/TagHelpers/ReproduceFlagInputTagHelper.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Database.Models;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace Website.TagHelpers
+{
+    /// <summary>
+    /// For checkboxes. Sets the checkbox's value to the given `ReproduceType`, and ensures the checkbox is pre-checked
+    /// if the `ReproduceInput` has the correct flag set for the given type.
+    /// </summary>
+    [HtmlTargetElement("input")]
+    public class ReproduceFlagInputTagHelper : TagHelper
+    {
+        public ReproduceFlags ReproduceType { get; set; }
+        public int ReproduceInput { get; set; } // This is from the Critter model, so it'll be an int.
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.Attributes.Add("value", (int)this.ReproduceType);
+
+            if((this.ReproduceInput & (int)this.ReproduceType) > 0)
+                output.Attributes.Add("checked", null);
+        }
+    }
+}
