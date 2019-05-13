fa1a340cceb7d528dba386ea8ad14a9ad45cf551
SealabJaster <sealabjaster@gmail.com>
2019-05-13 08:15:16 +0100

    Split the life event editors into their own controller. Closes #28

--- Livestock/Controllers/CritterExController.cs
+++ Livestock/Controllers/CritterExController.cs
@@ -258,104 +258,6 @@ namespace Website.Controllers
             return Json(breeds);
         }
         #endregion
-        
-        #region DateTime
-        [Authorize(Roles = "admin,staff")]
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> NewDateTime(int? id, [Bind("DateTime,EventTypeName,EventDescription")]CritterExEditViewModel model)
-        {
-            if(id == null)
-                throw new Exception("ID isn't being passed.");
-
-            using (var transact = await this._livestock.Database.BeginTransactionAsync())
-            {
-                var dateTime = new CritterLifeEventDatetime
-                {
-                    Comment         = "N/A",
-                    DateTime        = model.DateTime.DateTime,
-                    VersionNumber   = 1
-                };
-                await this._livestock.AddAsync(dateTime);
-                await this._livestock.SaveChangesAsync(); // We have to save here so dateTime gets an Id
-
-                await this.AddNewEvent(id.Value, dateTime.CritterLifeEventGiveBirthId, model.EventTypeName, model.EventDescription);
-                transact.Commit();
-            }
-
-            return RedirectToAction(nameof(Edit), new { id });
-        }
-        
-        public async Task<IActionResult> EditDateTime(int id)
-        {
-            var @event = await this._livestock.CritterLifeEvent.FindAsync(id);
-            var value = await this._livestock.CritterLifeEventDatetime.FindAsync(@event.EnumCritterLifeEventDataId);
-            return View(
-                new LifeEventEditDateTime
-                {
-                    Common = new LifeEventEditCommon
-                    {
-                        Id = id,
-                        Description = @event.Description
-                    },
-
-                    DateTime = value.DateTime
-                }
-            );
-        }
-
-        [Authorize(Roles = "admin,staff")]
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> EditDateTime([Bind("DateTime,Common")]LifeEventEditDateTime model)
-        {
-            var @event = await this._livestock.CritterLifeEvent.FindAsync(model.Common.Id);
-            var value = await this._livestock.CritterLifeEventDatetime.FindAsync(@event.EnumCritterLifeEventDataId);
-
-            @event.Description = model.Common.Description;
-            value.DateTime = model.DateTime;
-
-            await this._livestock.SaveChangesAsync();
-
-            return RedirectToAction(nameof(Edit), routeValues: new { id = model.Common.Id });
-        }
-
-        [Authorize(Roles = "admin,staff")]
-        public async Task<IActionResult> DeleteDateTime(int id)
-        {
-            // TODO: Make a function like "DateTimeFromId"
-            var @event = await this._livestock.CritterLifeEvent.FindAsync(id);
-            var value = await this._livestock.CritterLifeEventDatetime.FindAsync(@event.EnumCritterLifeEventDataId);
-            return View(
-                new LifeEventEditDateTime
-                {
-                    Common = new LifeEventEditCommon
-                    {
-                        Id = id,
-                        Description = @event.Description
-                    },
-
-                    DateTime = value.DateTime
-                }
-            );
-        }
-
-        [Authorize(Roles = "admin,staff")]
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> DeleteDateTimePost()
-        {
-            var id = Convert.ToInt32(Request.Form["Common.Id"]);
-            var @event = await this._livestock.CritterLifeEvent.FindAsync(id);
-            var value = await this._livestock.CritterLifeEventDatetime.FindAsync(@event.EnumCritterLifeEventDataId);
-
-            this._livestock.CritterLifeEvent.Remove(@event);
-            this._livestock.CritterLifeEventDatetime.Remove(value);
-            await this._livestock.SaveChangesAsync();
-
-            return RedirectToAction(nameof(Edit), routeValues: new { id });
-        }
-        #endregion
 
         #region Utility
         private void FixNullFields(Critter val)
@@ -365,22 +267,6 @@ namespace Website.Controllers
             if (String.IsNullOrWhiteSpace(val.Gender)) val.Gender = "?";
             if (String.IsNullOrWhiteSpace(val.MumFurther)) val.MumFurther = "N/A";
             if (String.IsNullOrWhiteSpace(val.Name)) val.Name = "N/A";
-        }
-
-        private async Task AddNewEvent(int critterId, int dataId, string eventTypeName, string description)
-        {
-            var @event = new CritterLifeEvent
-            {
-                CritterId = critterId,
-                Comment = "N/A",
-                DateTime = DateTime.Now,
-                Description = description,
-                EnumCritterLifeEventDataId = dataId,
-                EnumCritterLifeEventType = await this._livestock.EnumCritterLifeEventType.FirstAsync(e => e.Description == eventTypeName),
-                VersionNumber = 1
-            };
-            await this._livestock.AddAsync(@event);
-            await this._livestock.SaveChangesAsync();
         }
         #endregion
     }
---
+++ Livestock/Controllers/LifeEventController.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Database.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Website.Models;
+
+namespace Website.Controllers
+{
+    public class LifeEventController : Controller
+    {
+        readonly LivestockContext _livestock;
+
+        public LifeEventController(LivestockContext livestock)
+        {
+            this._livestock = livestock;
+        }
+
+        private async Task AddNewEvent(int critterId, int dataId, string eventTypeName, string description)
+        {
+            var @event = new CritterLifeEvent
+            {
+                CritterId = critterId,
+                Comment = "N/A",
+                DateTime = DateTime.Now,
+                Description = description,
+                EnumCritterLifeEventDataId = dataId,
+                EnumCritterLifeEventType = await this._livestock.EnumCritterLifeEventType.FirstAsync(e => e.Description == eventTypeName),
+                VersionNumber = 1
+            };
+            await this._livestock.AddAsync(@event);
+            await this._livestock.SaveChangesAsync();
+        }
+
+        #region DateTime
+        [Authorize(Roles = "admin,staff")]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> NewDateTime(int? id, [Bind("DateTime,EventTypeName,EventDescription")]CritterExEditViewModel model)
+        {
+            if (id == null)
+                throw new Exception("ID isn't being passed.");
+
+            using (var transact = await this._livestock.Database.BeginTransactionAsync())
+            {
+                var dateTime = new CritterLifeEventDatetime
+                {
+                    Comment = "N/A",
+                    DateTime = model.DateTime.DateTime,
+                    VersionNumber = 1
+                };
+                await this._livestock.AddAsync(dateTime);
+                await this._livestock.SaveChangesAsync(); // We have to save here so dateTime gets an Id
+
+                await this.AddNewEvent(id.Value, dateTime.CritterLifeEventGiveBirthId, model.EventTypeName, model.EventDescription);
+                transact.Commit();
+            }
+
+            return RedirectToAction(nameof(CritterExController.Edit), "CritterEx", new { id });
+        }
+
+        public async Task<IActionResult> EditDateTime(int id)
+        {
+            var @event = await this._livestock.CritterLifeEvent.FindAsync(id);
+            var value = await this._livestock.CritterLifeEventDatetime.FindAsync(@event.EnumCritterLifeEventDataId);
+            return View(
+                new LifeEventEditDateTime
+                {
+                    Common = new LifeEventEditCommon
+                    {
+                        Id = id,
+                        Description = @event.Description
+                    },
+
+                    DateTime = value.DateTime
+                }
+            );
+        }
+
+        [Authorize(Roles = "admin,staff")]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> EditDateTime([Bind("DateTime,Common")]LifeEventEditDateTime model)
+        {
+            var @event = await this._livestock.CritterLifeEvent.FindAsync(model.Common.Id);
+            var value = await this._livestock.CritterLifeEventDatetime.FindAsync(@event.EnumCritterLifeEventDataId);
+
+            @event.Description = model.Common.Description;
+            value.DateTime = model.DateTime;
+
+            await this._livestock.SaveChangesAsync();
+
+            return RedirectToAction(nameof(CritterExController.Index), "CritterEx");
+        }
+
+        [Authorize(Roles = "admin,staff")]
+        public async Task<IActionResult> DeleteDateTime(int id)
+        {
+            // TODO: Make a function like "DateTimeFromId"
+            var @event = await this._livestock.CritterLifeEvent.FindAsync(id);
+            var value = await this._livestock.CritterLifeEventDatetime.FindAsync(@event.EnumCritterLifeEventDataId);
+            return View(
+                new LifeEventEditDateTime
+                {
+                    Common = new LifeEventEditCommon
+                    {
+                        Id = id,
+                        Description = @event.Description
+                    },
+
+                    DateTime = value.DateTime
+                }
+            );
+        }
+
+        [Authorize(Roles = "admin,staff")]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteDateTimePost()
+        {
+            var id = Convert.ToInt32(Request.Form["Common.Id"]);
+            var @event = await this._livestock.CritterLifeEvent.FindAsync(id);
+            var value = await this._livestock.CritterLifeEventDatetime.FindAsync(@event.EnumCritterLifeEventDataId);
+
+            this._livestock.CritterLifeEvent.Remove(@event);
+            this._livestock.CritterLifeEventDatetime.Remove(value);
+            await this._livestock.SaveChangesAsync();
+
+            return RedirectToAction(nameof(CritterExController.Index), "CritterEx");
+        }
+        #endregion
+    }
+}
