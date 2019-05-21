9dd9d2f5d3a31175defceb466c2c7f9f09e5175d
SealabJaster <sealabjaster@gmail.com>
2019-05-21 20:56:58 +0100

    Fix dumb bug I put back in

--- Database/ModelExtentions/Critter.cs
+++ Database/ModelExtentions/Critter.cs
@@ -54,8 +54,8 @@ namespace Database.Models
         public bool HasFlag(CritterFlags flag) => (this.Flags & (int)flag) > 0;
 
         public bool CanReproduce =>
-            (this.Flags & (int)CritterFlags.ReproduceNoLifeEvent) > 0
-         && (this.Flags & (int)CritterFlags.ReproduceNoTooOld) > 0
+            (this.Flags & (int)CritterFlags.ReproduceNoLifeEvent) == 0
+         && (this.Flags & (int)CritterFlags.ReproduceNoTooOld) == 0
          && (this.Flags & (int)CritterFlags.ReproduceYesUser) > 0;
 
         public bool CanSafelyDelete(out string reasonIfFalse)
