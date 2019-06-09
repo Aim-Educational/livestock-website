0ff09d260702566e45adfe0bb4272995ca805ffb
SealabJaster <sealabjaster@gmail.com>
2019-06-09 03:54:00 +0100

    Add changelog

---
+++ Livestock/Controllers/ChangelogController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Website.Controllers
+{
+    public class ChangelogController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult ShowLog(string version, bool technical = false)
+        {
+            return View(version, technical);
+        }
+    }
+}
